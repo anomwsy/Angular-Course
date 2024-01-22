78c989986be0adefd533297c6ba41089bd1e32fa
anom wisnu syaputra <anom.syaputra@indocyber.id>
2024-01-22 13:53:03 +0700

    add dummy login data

--- src/app/modules/Auth/login/login.component.ts
+++ src/app/modules/Auth/login/login.component.ts
@@ -1,27 +1,51 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
-
+type LoginData = {
+  email: string;
+  password: string;
+};
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  loginData: LoginData[] = [];
   detailForm!: FormGroup;
-
-  constructor(private formBuilder: FormBuilder, private route: ActivatedRoute, private router: Router) {
+  errorMessage: string | null = null;
+  constructor(
+    private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {
     this.detailForm = this.createForm();
   }
 
-
+  ngOnInit(): void {
+    this.inputLoginData();
+    this.detailForm = this.createForm();
+  }
   onSubmit() {
     this.markFormGroupTouched(this.detailForm);
     if (this.detailForm.valid) {
       const value = this.detailForm.getRawValue();
-      localStorage.setItem('token', JSON.stringify(this.detailForm.getRawValue()));
-      console.log(value);
-      this.router.navigate(['/']);
+
+      if (value) {
+        this.loginData.forEach((data) => {
+          if (data.email === value.email && data.password === value.password) {
+            localStorage.setItem(
+              'token',
+              JSON.stringify(this.detailForm.getRawValue())
+            );
+            console.log(value);
+            this.router.navigate(['/']);
+            this.errorMessage = null;
+          } else {
+            this.errorMessage = 'Wrong email or password';
+          }
+        });
+      }
     }
   }
 
@@ -35,13 +59,36 @@ export class LoginComponent {
   }
   createForm() {
     return this.formBuilder.group({
-      email: ['', Validators.compose([Validators.required, Validators.email])],
-      password: ['', Validators.compose([Validators.required])],
+      email: ['martin@gmail.com', Validators.compose([Validators.required, Validators.email])],
+      password: ['123456', Validators.compose([Validators.required])],
       termAndCondition: ['', Validators.compose([Validators.requiredTrue])],
     });
   }
 
+  inputLoginData() {
+    const data = [
+      {
+        email: 'martin@gmail.com',
+        password: '123456',
+      },
+      {
+        email: 'priyo@gmail.com',
+        password: '123456',
+      },
+      {
+        email: 'aldy@gmail.com',
+        password: '123456',
+      },
+    ];
+    this.loginData = data;
+  }
 
+  onInputChange() {
+    // Sembunyikan pesan kesalahan saat nilai input berubah
+    if (this.errorMessage && this.errorMessage !== '') {
+      this.errorMessage = null;
+    }
+  }
 
   //  updateSelectedJutsu(event: any) {
   //   const checkbox = event.target;
